8f1e9628d7071f39083287a1a12152631fb81409
Aman Sharma <aman8696.56@gmail.com>
2020-06-28 02:25:00 +0530

    Setting default sorting ascending

--- src/components/StudentRecords.vue
+++ src/components/StudentRecords.vue
@@ -2,7 +2,12 @@
   <div>
     <el-row style="border: 1px solid lightgray;">
       <el-col>
-        <el-table :data="studentsData" @selection-change="handleSelectionChange" height="400">
+        <el-table
+          :data="studentsData"
+          :default-sort="{prop: 'age', order: 'ascending'}"
+          @selection-change="handleSelectionChange"
+          height="400"
+        >
           <el-table-column type="selection"></el-table-column>
           <el-table-column prop="name" label="Name"></el-table-column>
           <el-table-column sortable prop="age" label="Age" width="80"></el-table-column>
@@ -19,7 +24,7 @@
 </template>
 
 <script>
-import { STUDENTS_DATA } from '../data/students_data.js'
+import { STUDENTS_DATA } from "../data/students_data.js";
 
 export default {
   name: "StudentRecords",
@@ -31,7 +36,7 @@ export default {
   },
   methods: {
     handleSelectionChange(val) {
-      this.selectedStudents = val
+      this.selectedStudents = val;
     }
   }
 };
